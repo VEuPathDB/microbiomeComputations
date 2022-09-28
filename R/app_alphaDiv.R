574ebd79e6bea6ff25f63342215319c5bd0ce55c
Danielle Callan <dcallan@upenn.edu>
2022-09-28 15:21:35 -0400

    refactor alpha div and ranked abundance

--- R/app_alphaDiv.R
+++ R/app_alphaDiv.R
@@ -20,6 +20,20 @@ alphaDiv <- function(df, recordIdColumn, method = c('shannon','simpson','evennes
     naToZero <- veupathUtils::matchArg(naToZero)
     verbose <- veupathUtils::matchArg(verbose)
 
+    # Check that incoming df meets requirements
+    if (!'data.table' %in% class(df)) {
+      data.table::setDT(df)
+    }
+    if (!recordIdColumn %in% names(df)) {
+      stop("recordIdColumn must exist as a column in df")
+    }
+    if (!all(unlist(lapply(df[, -..recordIdColumn], is.numeric)))) {
+      stop("All columns except the recordIdColumn must be numeric")
+    }
+    if (uniqueN(veupathUtils::strSplit(names(df), ".", ncol=2, index=1)) > 1) {
+      stop("All entities must be identical")
+    }
+    
     computeMessage <- ''
     veupathUtils::logWithTime(paste("Received df table with", nrow(df), "samples and", (ncol(df)-1), "taxa."), verbose)
     
@@ -108,53 +122,4 @@ alphaDiv <- function(df, recordIdColumn, method = c('shannon','simpson','evennes
     veupathUtils::logWithTime(paste('Alpha diversity computation completed with parameters recordIdColumn=', recordIdColumn, ', method=', method, ', verbose =', verbose), verbose)
     
     return(dt)
-}
-
-
-#' Alpha diversity
-#'
-#' This function returns json with alpha diversity values for each sample.
-#' 
-#' @param df data.frame with samples as rows, taxa as columns
-#' @param recordIdColumn string defining the name of the df column that specifies sample ids. Note, all other columns must be numeric and will be treated as abundance values.
-#' @param methods vector of strings defining the the beta diversity dissimilarity methods to use. Must be a subset of c('shannon','simpson','evenness').
-#' @param naToZero boolean indicating if NAs in numeric columns should be replaced with 0
-#' @param verbose boolean indicating if timed logging is desired
-#' @return name of a json file containing a list of data.tables, one for each method specified in methods. Each data.table contains columns recordIdColumn, "alphaDiversity", and an attribute "parameters" that records the method used.
-#' @export
-#' @import data.table
-#' @import veupathUtils
-alphaDivApp <- function(df, recordIdColumn, methods = c('shannon','simpson','evenness'), naToZero = c(TRUE, FALSE), verbose = c(TRUE, FALSE)) {
-
-    naToZero <- veupathUtils::matchArg(naToZero)
-    verbose <- veupathUtils::matchArg(verbose)
-
-    # Allow any number of methods to be inputted
-    allMethods <- c('shannon','simpson','evenness')
-    if (is.null(methods)) methods <- allMethods
-    if (!all(methods %in% allMethods)) {
-      stop("Unaccepted method found in 'methods' argument. 'methods' must be a subset of c('shannon','simpson','evenness').")
-    }
-    
-    # Check that incoming df meets requirements
-    if (!'data.table' %in% class(df)) {
-      data.table::setDT(df)
-    }
-    if (!recordIdColumn %in% names(df)) {
-      stop("recordIdColumn must exist as a column in df")
-    }
-    if (!all(unlist(lapply(df[, -..recordIdColumn], is.numeric)))) {
-      stop("All columns except the recordIdColumn must be numeric")
-    }
-    if (uniqueN(veupathUtils::strSplit(names(df), ".", ncol=2, index=1)) > 1) {
-      stop("All entities must be identical")
-    }
-    
-
-    appResults <- lapply(methods, alphaDiv, df=df, recordIdColumn=recordIdColumn, naToZero=naToZero, verbose=verbose)
-
-    # Write to json. Note need to handle failures in here, too
-    # outFileName <- writeAppResultsToJson(appResults, recordIdColumn=recordIdColumn, 'alphaDiv', verbose = verbose)
-
-    return(appResults)
 }
